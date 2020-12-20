99947c250c054f157d9cab66df1ba69f19c5d240
hirafoo <hirafoo@gmail.com>
2020-12-20 15:10:19 +0900

    i

--- kotlin/hello.kt
+++ kotlin/hello.kt
@@ -1,7 +1,23 @@
 // kotlinc-jvm hello.kt -include-runtime -d app.jar
 // java -jar app.jar 
 
+fun hello() {
+    println("hoge")
 //fun main(args:Array<String>) {
 fun main() {
-    println("hoge")
+    hello()
+}
+
+fun st() {
+    var s: String = "hoge"
+    val t: String = "huga"
+
+    println(s)
+    println(t)
+
+    var a: Int = 10
+    val b: Boolean = true
+
+    println(a)
+    println(b)
 }
