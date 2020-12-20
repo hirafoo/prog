66990b951ddf7d8d99557226abbc274bdfe831a4
hirafoo <hirafoo@gmail.com>
2020-12-20 15:36:59 +0900

    i

--- kotlin/hello.kt
+++ kotlin/hello.kt
@@ -1,11 +1,14 @@
 // kotlinc-jvm hello.kt -include-runtime -d app.jar
-// java -jar app.jar 
+// java -jar app.jar
 
-fun hello() {
-    println("hoge")
 //fun main(args:Array<String>) {
 fun main() {
     hello()
+    st()
+}
+
+fun hello() {
+    println("hoge")
 }
 
 fun st() {
@@ -20,4 +23,7 @@ fun st() {
 
     println(a)
     println(b)
+
+    val st: String = a.toString()
+    println("hoge " + st)
 }
